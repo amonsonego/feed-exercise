14614ff66116a23d4bc6b11dd928ec802642bf81
adamonso <amonsonego@lightricks.com>
2021-11-07 12:01:05 +0200

    Implement api service using retrofit

--- app/src/main/java/com/lightricks/feedexercise/network/DataTransferObjects.kt
+++ app/src/main/java/com/lightricks/feedexercise/network/DataTransferObjects.kt
@@ -1,5 +1,28 @@
 package com.lightricks.feedexercise.network
 
-/**
- * todo: add Data Transfer Object data class(es) here
- */
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class FeedMetadataDto(
+    @Json(name ="templatesMetadata")
+    val templatesMetadata: List<FeedItemDto>
+)
+
+@JsonClass(generateAdapter = true)
+data class FeedItemDto(
+    @Json(name = "configuration")
+    val configuration: String,
+    @Json(name = "id")
+    val id: String,
+    @Json(name = "isNew")
+    val isNew: Boolean,
+    @Json(name = "isPremium")
+    val isPremium: Boolean,
+    @Json(name = "templateCategories")
+    val templateCategories: List<String>,
+    @Json(name = "templateName")
+    val templateName: String,
+    @Json(name = "templateThumbnailURI")
+    val templateThumbnailURI: String
+)
--- app/src/main/java/com/lightricks/feedexercise/network/FeedApiService.kt
+++ app/src/main/java/com/lightricks/feedexercise/network/FeedApiService.kt
@@ -1,5 +1,33 @@
 package com.lightricks.feedexercise.network
 
-/**
- * todo: add the FeedApiService interface and the Retrofit and Moshi code here
- */
+import retrofit2.http.GET
+import retrofit2.Retrofit
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import io.reactivex.rxjava3.core.Single
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+interface FeedApiService {
+    @GET(FEED_QUERY)
+    fun getFeed(): Single<FeedMetadataDto>
+
+    companion object {
+        private const val FEED_QUERY = "feed.json"
+        private const val BASE_URL = "https://assets.swishvideoapp.com/Android/demo/"
+        const val THUMBNAILS_URL_PREFIX = BASE_URL + "catalog/thumbnails/"
+
+        private val moshi: Moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        private val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+
+        val feedApiService: FeedApiService = retrofit.create(FeedApiService::class.java)
+    }
+}
