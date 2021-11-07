94b144e7d0c1d1f37501afc56799d5c66077160c
adamonso <amonsonego@lightricks.com>
2021-11-07 12:02:36 +0200

    Implement cache database using room

--- app/src/main/java/com/lightricks/feedexercise/database/Entities.kt
+++ app/src/main/java/com/lightricks/feedexercise/database/Entities.kt
@@ -1,5 +1,15 @@
 package com.lightricks.feedexercise.database
 
-/**
- * todo: add Room's Entity data class(es) here
- */
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "feed_items")
+data class FeedItemEntity(
+    @ColumnInfo val configuration: String,
+    @PrimaryKey val feedId: String,
+    @ColumnInfo val isNew: Boolean,
+    @ColumnInfo val isPremium: Boolean,
+    @ColumnInfo val templateName: String,
+    @ColumnInfo val templateThumbnailURI: String
+)
